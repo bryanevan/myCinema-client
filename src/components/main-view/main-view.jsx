0a227a75c7d694a7442cf70f1a771b29dcaf910d
bryanevan <35902530+bryanevan@users.noreply.github.com>
2023-03-27 17:13:37 -1000

    3.5 create signup/login view (#3)
    Added signup-view.jsx & login-view.jsx to facilitate new user reg and logging in.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,49 +1,114 @@
-import {useState, useEffect} from "react";
-import {MovieCard} from "../movie-card/movie-card";
-import {MovieView} from "../movie-view/movie-view";
+
+
+import { useState, useEffect } from "react";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view"; 
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
-    const [movies, setMovies] = useState([]);
-    const [selectedMovie, setSelectedMovie] = useState(null);
-
-    useEffect(() => {
-        fetch("https://mycinema.herokuapp.com/movies")
-        .then((response) => response.json())
-        .then((data) => {
-            const moviesFromApi = data.map((movie) => {
-                const obj = {id: movie._id, 
-                            title: movie.Title, 
-                            image: movie.imageUrl,
-                            director: movie.Director.Name, 
-                            genre: movie.Genre.Name,
-                            description: movie.Description}
-                return obj;
-            });
-            setMovies(moviesFromApi);
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(storedUser? storedUser : null);
+  const [token, setToken] = useState(storedToken? storedToken : null);
+  const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [loading, setLoading] = useState(false);
+  
+  // useEffect hook allows React to perform side effects in component e.g fetching data
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+    // set loading before sending API request
+    setLoading(true);
+    fetch("https://mycinema.herokuapp.com/movies", {
+      headers: {Authorization: `Bearer ${token}`}
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // stops loading after response received
+        setLoading(false);
+        console.log('data', data);
+        const moviesFromApi = data.map((movie) => {
+          return {
+          // value names match to API database
+          id: movie._id,
+          title: movie.Title,
+          image: movie.ImagePath,
+          description: movie.Description,
+          genre: movie.Genre.Name,
+          director: movie.Director.Name,
+          release: movie.Release
+          }
         });
-    },[]);
-        
-    if (selectedMovie) {
-        return (
-          <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
-        );
-      }
+        setMovies(moviesFromApi);
+      })
+  }, [token])
+
+  // user must first either login or signup
+  if (!user) {
+    return (
+      <>
+        <LoginView onLoggedIn={(user, token) => {
+          setUser(user);
+          setToken(token);
+        }} />
+        or
+        <SignupView />
+      </>
+    )
+  }
+
+  // displays movie-view when movie is selected (clicked)
+  if (selectedMovie) {
+    return (
+      <>
+      <button onClick={() => { setUser(null); setToken(null); localStorage.clear();
+      }}
+      > Logout 
+      </button>
+      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+      </>
+    );
+  }
+
+  // displays text message if list of movies is empty
+  if (movies.length === 0) {
+    return (
+      <>
+      <button onClick={() => { setUser(null); setToken(null); localStorage.clear();
+      }}
+      > Logout
+      </button>
+      <div>The list is empty!</div>
+    </>
+    );
+  }
+
+  // displays movie-card with logout button, if user does not select a movie 
+  return (
+    // conditional rendering for loading statment
+    loading ? (
+      <p>Loading...</p>
+    ) : !movies || !movies.length ? (
+      <p>No movies found</p>
+    ) : (
+    <div>
+      <button onClick={() => { setUser(null); setToken(null); localStorage.clear();
+      }}
+    > Logout
+    </button>
     
-      if (movies.length === 0) {
-        return <div>The list is empty!</div>;
-      }
-    
-      return (
-        <div>
-          {movies.map((movie) => (
-            <MovieCard
-              key={movie.id}
-              movie={movie}
-              onMovieClick={(newSelectedMovie) => {
-                setSelectedMovie(newSelectedMovie);
-              }}
-            />
-          ))}
-        </div>
-      );
-    };
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie._id}
+          movie={movie}
+          onMovieClick={(newSelectedMovie) => {
+            setSelectedMovie(newSelectedMovie);
+          }}
+        />
+      ))}
+    </div>
+  ));
+}
